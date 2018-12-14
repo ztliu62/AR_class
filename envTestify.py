41f7f52657457eefd62b0ca2f38180699cf58a3e
ztliu62 <30788879+ztliu62@users.noreply.github.com>
2018-12-14 10:00:24 -0500

    Update envTestify.py

--- envTestify.py
+++ envTestify.py
@@ -4,7 +4,7 @@ import sys
 
 print "The Python version is %s.%s.%s" % sys.version_info[:3]
 
-img = cv2.imread('./images/black.jpg', 0)
+img = cv2.imread('./Images/black.jpg', 0)
 rows, cols = img.shape[:2]
 #print rows, cols
 if (rows == 600 and cols == 800):
